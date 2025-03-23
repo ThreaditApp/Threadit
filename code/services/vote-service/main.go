0b0627710d02f6b0ce58b4e37ce19478137c46fd
Ricardo Costa <rcosta.ms358@gmail.com>
2025-03-23 19:43:21 +0000

    Add gRPC Client Connections in Vote Service

--- code/services/vote-service/main.go
+++ code/services/vote-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -8,14 +9,37 @@ import (
 	pb "vote-service/src/pb"
 )
 
+func ConnectGrpcClient(serviceName string, port int) *grpc.ClientConn {
+	addr := fmt.Sprintf("localhost:%d", port)
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to %s: %v", serviceName, err)
+	}
+	return conn
+}
+
 func main() {
+
+	// connect to other services
+	threadConn := ConnectGrpcClient("thread service", 50056)
+	defer threadConn.Close()
+	commentConn := ConnectGrpcClient("comment service", 50057)
+	defer commentConn.Close()
+
+	// create social service with database service
+	voteService := &server.VoteServer{
+		ThreadClient:  threadpb.NewThreadServiceClient(threadConn),
+		CommentClient: commentpb.NewCommentServiceClient(commentConn),
+	}
+
+	// start gRPC server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterVoteServiceServer(grpcServer, &server.VoteServer{})
+	pb.RegisterVoteServiceServer(grpcServer, voteService)
 
 	log.Println("gRPC server is listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {
--- code/services/vote-service/src/server.go
+++ code/services/vote-service/src/server.go
@@ -1,35 +1,99 @@
 package server
 
 import (
+	"fmt"
 	"context"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
 	pb "vote-service/src/pb"
 )
 
 type VoteServer struct {
 	pb.UnimplementedVoteServiceServer
+	ThreadClient threadpb.ThreadServiceClient
+	CommentClient commentpb.CommentServiceClient
 }
 
 func (s *VoteServer) UpvoteThread(ctx context.Context, req *pb.VoteThreadRequest) (*emptypb.Empty, error) {
+	_, err := s.ThreadClient.UpdateVote(ctx, &threadpb.VoteThreadRequest{
+		ThreadId: req.ThreadId,
+		UserId: getCurrentUserId(ctx),
+		Value: 1,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *VoteServer) DownvoteThread(ctx context.Context, req *pb.VoteThreadRequest) (*emptypb.Empty, error) {
+	_, err := s.ThreadClient.UpdateVote(ctx, &threadpb.VoteThreadRequest{
+		ThreadId: req.ThreadId,
+		UserId: getCurrentUserId(ctx),
+		Value: -1,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *VoteServer) RemoveThreadVote(ctx context.Context, req *pb.VoteThreadRequest) (*emptypb.Empty, error) {
+	_, err := s.ThreadClient.UpdateVote(ctx, &threadpb.VoteThreadRequest{
+		ThreadId: req.ThreadId,
+		UserId: getCurrentUserId(ctx),
+		Value: 0,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *VoteServer) UpvoteComment(ctx context.Context, req *pb.VoteCommentRequest) (*emptypb.Empty, error) {
+	_, err := s.CommentClient.UpdateVote(ctx, &commentpb.VoteCommentRequest{
+		CommentId: req.CommentId,
+		UserId: getCurrentUserId(ctx),
+		Value: 1,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *VoteServer) DownvoteComment(ctx context.Context, req *pb.VoteCommentRequest) (*emptypb.Empty, error) {
+	_, err := s.CommentClient.UpdateVote(ctx, &commentpb.VoteCommentRequest{
+		CommentId: req.CommentId,
+		UserId: getCurrentUserId(ctx),
+		Value: -1,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *VoteServer) RemoveCommentVote(ctx context.Context, req *pb.VoteCommentRequest) (*emptypb.Empty, error) {
+	_, err := s.CommentClient.UpdateVote(ctx, &commentpb.VoteCommentRequest{
+		CommentId: req.CommentId,
+		UserId: getCurrentUserId(ctx),
+		Value: 0,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling database service: %w", err)
+	}
 	return &emptypb.Empty{}, nil
+}
+
+func getCurrentUserId(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("no metadata found in context")
+	}
+	userIds := md.Get("x-user-id")
+	if len(userIds) == 0 {
+		return "", fmt.Errorf("user id not found in metadata")
+	}
+	return userIds[0], nil
 }
