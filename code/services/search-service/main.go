ee48f21e79e582ef4d892ee5491b5bd605de33d3
Ricardo Costa <rcosta.ms358@gmail.com>
2025-03-23 19:23:30 +0000

    Add gRPC Client Connections in Search Service

--- code/services/search-service/main.go
+++ code/services/search-service/main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
 	server "search-service/src"
 	pb "search-service/src/pb"
 )
 
+func ConnectGrpcClient(serviceName string, port int) *grpc.ClientConn {
+	addr := fmt.Sprintf("localhost:%d", port)
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to %s: %v", serviceName, err)
+	}
+	return conn
+}
+
 func main() {
+	// connect to other services
+	userConn := ConnectGrpcClient("user service", 50052)
+	defer userConn.Close()
+	communityConn := ConnectGrpcClient("community service", 50053)
+	defer communityConn.Close()
+	threadConn := ConnectGrpcClient("thread service", 50054)
+	defer threadConn.Close()
+
+	// create search server with clients
+	searchServer := &server.SearchServer{
+		UserClient:      userpb.NewUserServiceClient(userConn),
+		CommunityClient: communitypb.NewCommunityServiceClient(communityConn),
+		ThreadClient:    threadpb.NewThreadServiceClient(threadConn),
+	}
+
+	// start gRPC server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterSearchServiceServer(grpcServer, &server.SearchServer{})
+	pb.RegisterSearchServiceServer(grpcServer, searchServer)
 
-	log.Println("gRPC server is listening on :50051")
+	log.Println("search service is listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
--- code/services/search-service/src/server.go
+++ code/services/search-service/src/server.go
@@ -1,26 +1,76 @@
 package server
 
 import (
+	"fmt"
 	"context"
 	pb "search-service/src/pb"
 )
 
 type SearchServer struct {
 	pb.UnimplementedSearchServiceServer
+	UserClient userpb.UserServiceClient
+	CommunityClient communitypb.CommunityServiceClient
+	ThreadClient threadpb.ThreadServiceClient
 }
 
 func (s *SearchServer) GlobalSearch(ctx context.Context, req *pb.GlobalSearchRequest) (*pb.GlobalSearchResponse, error) {
-	return &pb.GlobalSearchResponse{}, nil
+	userRes, err := s.UserClient.GetUsersByName(ctx, &userpb.UserQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling user service: %w", err)
+	}
+	communityRes, err := s.CommunityClient.GetCommunitiesByName(ctx, &communitypb.CommunityQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling community service: %w", err)
+	}
+	threadRes, err := s.ThreadClient.GetThreadsByTitle(ctx, &threadpb.ThreadQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling thread service: %w", err)
+	}
+	return &pb.GlobalSearchResponse{
+		UserIds: userRes.UserIds,
+		CommunityIds: communityRes.CommunityIds,
+		ThreadIds: threadRes.ThreadIds,
+	}, nil
 }
 
 func (s *SearchServer) UserSearch(ctx context.Context, req *pb.UserSearchRequest) (*pb.UserSearchResponse, error) {
-	return &pb.UserSearchResponse{}, nil
+	res, err := s.UserClient.GetUsersByName(ctx, &userpb.UserQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling user service: %w", err)
+	}
+	return &pb.UserSearchResponse{
+		UserIds: res.UserIds,
+	}, nil
 }
 
 func (s *SearchServer) CommunitySearch(ctx context.Context, req *pb.CommunitySearchRequest) (*pb.CommunitySearchResponse, error) {
-	return &pb.CommunitySearchResponse{}, nil
+	res, err := s.UserClient.GetCommunitiesByName(ctx, &communitypb.CommunityQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling community service: %w", err)
+	}
+	return &pb.CommunitySearchResponse{
+		CommunityIds: res.CommunityIds,
+	}, nil
 }
 
 func (s *SearchServer) ThreadSearch(ctx context.Context, req *pb.ThreadSearchRequest) (*pb.ThreadSearchResponse, error) {
-	return &pb.ThreadSearchResponse{}, nil
+	res, err := s.UserClient.GetThreadsByTitle(ctx, &threadpb.ThreadQueryRequest{
+		Query: req.Query,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling thread service: %w", err)
+	}
+	return &pb.ThreadSearchResponse{
+		ThreadIds: res.ThreadIds,
+	}, nil
 }
