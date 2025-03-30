b5c0dca8f299a772871e01ae8ce947030babf458
Ricardo Costa <rcosta.ms358@gmail.com>
2025-03-30 15:13:47 +0100

    Search & Vote Services Input Validations

--- code/services/vote-service/src/server.go
+++ code/services/vote-service/src/server.go
@@ -5,6 +5,8 @@ import (
 	commentpb "gen/comment-service/pb"
 	threadpb "gen/thread-service/pb"
 	votepb "gen/vote-service/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -39,6 +41,9 @@ func (s *VoteServer) RemoveCommentVote(ctx context.Context, req *votepb.VoteComm
 }
 
 func (s *VoteServer) updateThreadVote(ctx context.Context, req *votepb.VoteThreadRequest, value int32) (*emptypb.Empty, error) {
+	if req.GetThreadId() == "" {
+		return nil, status.Error(codes.InvalidArgument, "invalid thread id")
+	}
 	_, err := s.ThreadClient.UpdateThread(ctx, &threadpb.UpdateThreadRequest{
 		Id:         req.ThreadId,
 		VoteOffset: &value,
@@ -50,6 +55,9 @@ func (s *VoteServer) updateThreadVote(ctx context.Context, req *votepb.VoteThrea
 }
 
 func (s *VoteServer) updateCommentVote(ctx context.Context, req *votepb.VoteCommentRequest, value int32) (*emptypb.Empty, error) {
+	if req.GetCommentId() == "" {
+		return nil, status.Error(codes.InvalidArgument, "invalid comment id")
+	}
 	_, err := s.CommentClient.UpdateComment(ctx, &commentpb.UpdateCommentRequest{
 		Id:         req.CommentId,
 		VoteOffset: &value,
